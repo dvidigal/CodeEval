05e4ae718c6e132837eb5ee118c99d2836f30469
dvidigal <diogosf.vidigal@gmail.com>
2015-05-26 19:54:33 +0100

    SumOfIntegersfromFile.java

---
+++ Challenges/SumOfIntegersfromFile.java
@@ -0,0 +1,17 @@
+//Print out the sum of integers read from a file.
+
+import java.io.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+        int total = 0;
+        while ((line = buffer.readLine()) != null) {
+            line = line.trim();
+            total += Integer.parseInt(line);
+        }
+        System.out.println(total);
+    }
+}

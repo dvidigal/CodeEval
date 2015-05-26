416c5241cda8ff691bd59f7b30d42b0e3a38a828
dvidigal <diogosf.vidigal@gmail.com>
2015-05-26 23:38:32 +0100

    FileSize.java

---
+++ Challenges/FileSize.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File(args[0]);
+        System.out.println(file.length());
+    }
+}

b000f1058e3773fda50fb83fe15638e80cd54f64
dvidigal <diogosf.vidigal@gmail.com>
2015-05-26 23:33:30 +0100

    OddNumbers.java

---
+++ Challenges/OddNumbers.java
@@ -0,0 +1,11 @@
+//Print the odd numbers from 1 to 99.
+
+public class Main {
+    public static void main (String[] args) {
+        for(int i = 1; i < 100; i++) {
+            if(i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
